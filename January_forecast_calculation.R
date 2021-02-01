4b05b5dc553228a07d025ed81dae5311b30fe72b
Alex Young <bearsofthemoss@gmail.com>
2021-01-31 22:43:13 -0500

    Alex's new clean branch

---
+++ January_forecast_calculation.R
@@ -0,0 +1,148 @@
+
+library(neonUtilities)
+library(neonstore)
+library(tidyverse)
+library(lubridate)
+library(contentid)
+library(data.table)
+
+install.packages("EML")
+
+# Thanks to EFI, the target variables that have been passed through a QA filter are available here. 
+#The NEE, LE, and Soil moisture are contained with this dataet. 
+half<-fread("data_folder/targets-terrestrial/terrestrial_30min-targets.csv.gz")
+daily<-fread("data_folder/targets-terrestrial/terrestrial_daily-targets.csv.gz")
+
+
+tail(daily) # goes to december 2020
+
+daily$month<-month(daily$time)
+daily$day<-day(daily$time)
+daily$year<-year(daily$time)
+
+jan<-daily[daily$month=="1",]
+feb<-daily[daily$month=="2",]
+dec<-daily[daily$month=="12",]
+wind<-rbind(jan,feb,dec)
+ja<-gather(wind,"target","value",3:5)
+
+a<-ggplot(ja, aes(x=day, y=value, shape=as.factor(month), col=as.factor(year)))+geom_point()+
+  facet_grid(target~siteID, scales="free_y")+ggtitle("Daily")
+a
+############
+
+## Now for half hourly data
+
+ha<-gather(half,"target","value",c(3,4,11))
+names(ha)
+ha<-ha[ ,c(1,2,10,11)]
+
+ha$month<-month(ha$time)
+ha$day<-day(ha$time)
+ha$year<-year(ha$time)
+head(ha,200)
+
+Jan<-ha[ha$month==1,]
+
+
+b<-ggplot(Jan, aes(x=day, y=value, shape=as.factor(month), col=as.factor(year)))+geom_point()+
+  facet_grid(target~siteID, scales="free_y")+ggtitle("Half-hour")
+b
+
+## calculate average day from the half hours
+da<-aggregate(list(value=Jan$value), by=list(siteID=Jan$siteID, target=Jan$target,day=Jan$day),FUN="mean", na.rm=T)
+head(da)
+
+# do standard deviation! for each day
+st.err <- function(x) {  sd(x, na.rm=T)  }
+SE <- aggregate( list(se=Jan$value), by=list(siteID=Jan$siteID, target=Jan$target,day=Jan$day),FUN=  st.err)
+head(SE)
+da$se<-SE$se
+
+
+c<-ggplot(da, aes(x=day, y=value))+  geom_point()+
+  geom_errorbar(aes(ymin=value-se, ymax=value+se), width=.2)+
+  facet_grid(target~siteID, scales="free_y")+ggtitle("Avg+SD of half hour historical January data")
+c
+
+
+
+##### MONTH AVG
+month<-aggregate(list(value=da$value), by=list(siteID=da$siteID,target=da$target),FUN="mean", na.rm=T)
+
+## Here is where I might want to do 95% confidence?
+mse<-aggregate(list(se=da$se), by=list(siteID=da$siteID, target=da$target),FUN="mean", na.rm=T)
+head(mse)
+
+
+
+month$site.target<-paste(month$siteID, month$target)
+mse$site.target<-paste(mse$siteID, mse$target)
+
+month$se<-mse$se[match(month$site.target, mse$site.target)]
+
+
+# match in the monthly values into ja
+ja$m.avg<-month$value[match(ja$site.target, month$site.target)]
+ja$se<-month$se[match(ja$site.target, mse$site.target)]
+
+# only 1 year
+y21<-ja[ja$year=="2020",]
+
+d<-ggplot(y21, aes(x=day, y=m.avg))+  geom_point()+
+  geom_errorbar(aes(ymin=m.avg-se, ymax=m.avg+se), width=.2)+
+  facet_grid(target~siteID, scales="free_y")+ggtitle("Monthly average from half hours")
+d
+
+
+# see the 4 options and workflow
+library(ggpubr)
+ggarrange(a,b,c,d, common.legend = T, nrow=1)
+
+
+
+names(ja)
+us<-y21
+use<-us[,c(1,2,5,6,7,10)]
+use.se<-us[,c(1,2,5,6,7,11)]
+
+
+
+stat.mean<-spread(use[,c(1,2,5,6)], "target","m.avg")
+stat.mean$statistic<-"mean"
+stat.mean[stat.mean$siteID=="BART","vswc"]<-"0"
+head(stat.mean,20)
+
+names(use.se)
+
+stat.se<-spread(use.se[,c(1,2,5,6)], "target","se")
+stat.se$statistic<-"sd"
+stat.se[stat.se$siteID=="BART","vswc"]<-"0.05"
+head(stat.se)
+
+fin<-rbind(stat.mean, stat.se)
+fin$forecast<-"1"
+fin$data_assimilation<-"0"
+names(fin)
+head(fin)
+
+## pick which rows you want to keep using
+#final<-fin[,c("siteID","statistic","forecast","data_assimilation","time","nee","le","vcwc")]
+final<-fin[,c(2,8,9,10,1,6,5,7)]
+
+# too many rows!
+fin<-final[1:140,]
+fins<-final[365:504,]
+
+fi<-rbind(fin, fins)
+fi$time<-ymd(fi$time)
+fi$year<-2021
+fi$month<-month(fi$time)
+fi$day<-day(fi$time)
+
+fi$date<-paste(fi$year, fi$month, fi$day, sep="-")
+fi$time<-ymd(fi$date)
+names(fi)
+fi<-fi[,1:8]
+
+write.csv(fi, file="terrestrial-2021-01-01-ISWG.csv")
---
+++ generate_metadata.R
@@ -0,0 +1,129 @@
+library(EML)
+library(emld)
+emld::eml_version("eml-2.2.0")
+library(lubridate)
+remotes::install_github("eco4cast/EFIstandards")
+library(EFIstandards)
+
+# AY makes a forecast file for the name of the .csv
+
+
+generate_metadata <- function(forecast_file, metadata_yaml, forecast_issue_time, forecast_iteration_id, forecast_file_name_base,start_time=NULL,stop_time=NULL){
+  
+  metadata <- yaml::read_yaml(metadata_yaml)
+  
+  if(tools::file_ext(forecast_file) == "csv"){
+    forecast <- readr::read_csv()
+  } else {
+    forecast <- NULL
+  }
+  
+  theme <- unlist(stringr::str_split(stringr::str_split(forecast_file, "-")[[1]][1], "_")[[1]][1])
+  team_name <- unlist(stringr::str_split(unlist(stringr::str_split(forecast_file, "-"))[5], ".csv"))[1]
+  
+  if(theme == "terrestrial"){
+    #'Build attribute table. Other models won't likely change this for the challenge
+    #'note: need to fix the units for nee and le because the units do not pass EML unit checks
+    attributes <- tibble::tribble(
+      ~attributeName,     ~attributeDefinition,                          ~unit,                  ~formatString,  ~definition, ~numberType,
+      "time",              "[dimension]{time}",                          "year",                 "YYYY-MM-DD",   NA,          "datetime",
+      "ensemble",          "[dimension]{index of ensemble member}",      "dimensionless",         NA,            NA,          "integer",
+      "siteID",             "[dimension]{neon site}",                     NA,                     NA,           "NEON site ID",  "character",
+      "obs_flag",          "[flag]{observation error}",                  "dimensionless",         NA,           NA,           "integer",
+      "nee",               "[variable]{net ecosystem exchange}",         "dimensionless",         NA,           NA,           "real",
+      "le",                "[variable]{latent heat}",                    "dimensionless",         NA,           NA,          "real",
+      "vswc",              "[variable]{volumetric soil water content}",  "dimensionless",         NA,           NA,           "real",
+      "forecast",          "[flag]{whether represents forecast}",        "dimensionless",         NA,           NA,          "integer",
+      "data_assimilation", "[flag]{whether time step assimilated data}", "dimensionless",         NA,           NA,          "integer"
+    )
+    
+    if(!is.null(forecast)){
+      if(all.equal(names(forecast), attributes$attributeName) != TRUE){
+        message("Column names in file do not match required names for complete metadate")
+        message(paste0("File names are: ",names(forecast)))
+        message(paste0("Required names are: ",attributes$attributeName))
+        stop()
+      }
+    }
+    
+    #' use `EML` package to build the attribute list
+    attrList <- EML::set_attributes(attributes, 
+                                    col_classes = c("Date", "numeric", "character","numeric","numeric", 
+                                                    "numeric","numeric", "numeric","numeric"))
+    
+    entityDescription_text = "Forecast of NEE and LE for four NEON sites"
+  }
+  
+  #' use `EML` package to build the physical list
+  physical <- EML::set_physical(forecast_file)
+  
+  #' use `EML` package to dataTable
+  dataTable <- eml$dataTable(
+    entityName = "forecast",  ## this is a standard name to allow us to distinguish this entity from 
+    entityDescription = entityDescription_text,
+    physical = physical,
+    attributeList = attrList)
+  
+  fullgeographicCoverage <- jsonlite::read_json("meta/terrestrial_geo.json")
+  
+  site_id_index <- NULL
+  for(i in 1:length(fullgeographicCoverage)){
+    if(fullgeographicCoverage[[i]]$id %in% metadata$sites)
+      site_id_index <- c(site_id_index, i)
+  }
+  
+  geographicCoverage <- fullgeographicCoverage[site_id_index]
+  
+  if(is.null(start_time)) start_time = min(forecast$time)
+  if(is.null(stop_time)) stop_time = max(forecast$time)
+  
+  temporalCoverage <- list(rangeOfDates =
+                             list(beginDate = list(calendarDate = start_time),
+                                  endDate = list(calendarDate = stop_time)))
+  #'Create the coverage EML
+  coverage <- list(geographicCoverage = geographicCoverage,
+                   temporalCoverage = temporalCoverage)
+  
+  
+  
+  
+  #'Create the dataset EML
+  dataset <- eml$dataset(
+    title = "Daily persistence null forecast for nee and lee",
+    creator = metadata$team_list,
+    contact = metadata$team_list[[1]],
+    pubDate = as_date(forecast_issue_time),
+    intellectualRights = "https://creativecommons.org/licenses/by/4.0/",
+    dataTable = dataTable,
+    coverage = coverage
+  )
+  
+  metadata$metadata$forecast$forecast_issue_time <- as_date(forecast_issue_time)
+  metadata$metadata$forecast$forecast_iteration_id <- forecast_iteration_id
+  metadata$metadata$forecast$forecast_project_id <- team_name
+  
+  
+  my_eml <- eml$eml(dataset = dataset,
+                    additionalMetadata = eml$additionalMetadata(metadata = metadata$metadata),
+                    packageId = forecast_iteration_id , 
+                    system = "datetime"  ## system used to generate packageId
+  )
+  
+  
+  #'Check base EML
+  if(!EML::eml_validate(my_eml)){
+    message("Error in EML metadata")
+  }
+  
+  #'Check that EML matches EFI Standards
+  if(EFIstandards::forecast_validator(my_eml)){
+    #'Write metadata
+    meta_data_filename <-  paste0(forecast_file_name_base,".xml")
+    EML::write_eml(my_eml, meta_data_filename)
+  }else{
+    message("Error in EFI metadata")
+  }
+  return(meta_data_filename)
+}
+
+generate_metadata("terrestrial-2021-01-01-ISWG.csv", "metadata.yml", "2021-01-29", "80808080", "terrestrial-2021-01-01-ISWG", start_time=NULL,stop_time=NULL)
