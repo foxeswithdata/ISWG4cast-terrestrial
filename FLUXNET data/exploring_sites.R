58ba50cefaf6adb624ef7b335a4dd0986ae4e038
Kathryn Fuller <90946112@westernsydney.edu.au>
2021-04-08 16:19:25 -0500

    Update with better site selection

--- FLUXNET data/exploring_sites.R
+++ FLUXNET data/exploring_sites.R
@@ -1,3 +1,8 @@
 library(sf)
 library(tmap)
 library(tidyverse)
+
+sites = read.csv("FLUXNET data/sitelocations.csv") %>% 
+  st_as_sf(coords = c(lat = "lon", lon = "lat"), crs = 4326)
+tmap_mode("view")
+tm_shape(sites[sites$network == "NEON",])+tm_dots(col = "blue", size = 0.25)+tm_shape(sites[sites$network == "FLUXNET",])+tm_dots(col = "red", size = 0.25)
--- FLUXNET_NEE.R
+++ FLUXNET_NEE.R
@@ -3,32 +3,45 @@ setwd("./FLUXNET data")
 library(tidyverse)
 library(scales)
 
-# Harvard Forest- closest to NEON site BART ####
-Ha_weekly = read.csv("FLX_US-Ha1_FLUXNET2015_SUBSET_1991-2012_1-4/FLX_US-Ha1_FLUXNET2015_SUBSET_WW_1991-2012_1-4.csv") %>% 
-  dplyr::select(TIMESTAMP_START, NEE_VUT_REF, NEE_VUT_REF_QC, NEE_VUT_REF_RANDUNC) %>% 
-  filter(NEE_VUT_REF_QC != 0)
-Ha_weekly$site = "Harvard"
-Ks_weekly = read.csv("FLX_US-KS1_FLUXNET2015_SUBSET_2002-2002_1-4/FLX_US-KS1_FLUXNET2015_SUBSET_WW_2002-2002_1-4.csv") %>% 
-  dplyr::select(TIMESTAMP_START, NEE_VUT_REF, NEE_VUT_REF_QC, NEE_VUT_REF_RANDUNC) %>% 
-  filter(NEE_VUT_REF_QC != 0)
-Ks_weekly$site = "Kennedy"
-Ne_weekly = read.csv("FLX_US-Ne3_FLUXNET2015_SUBSET_2001-2013_1-4/FLX_US-Ne3_FLUXNET2015_SUBSET_WW_2001-2013_1-4.csv") %>% 
-  dplyr::select(TIMESTAMP_START, NEE_VUT_REF, NEE_VUT_REF_QC, NEE_VUT_REF_RANDUNC) %>% 
-  filter(NEE_VUT_REF_QC != 0)
-Ne_weekly$site = "Mead"
-SR_weekly = read.csv("FLX_US-SRC_FLUXNET2015_SUBSET_2008-2014_1-4/FLX_US-SRC_FLUXNET2015_SUBSET_WW_2008-2014_1-4.csv") %>% 
-  dplyr::select(TIMESTAMP_START, NEE_VUT_REF, NEE_VUT_REF_QC, NEE_VUT_REF_RANDUNC) %>% 
-  filter(NEE_VUT_REF_QC != 0)
-SR_weekly$site = "SantaRita"
-all_weekly = rbind(Ha_weekly, Ks_weekly, Ne_weekly, SR_weekly)
-head(all_weekly)
-all_weekly$TIMESTAMP_START = as.Date(as.character(all_weekly$TIMESTAMP_START), format = "%Y%m%d")
-all_weekly$week = strftime(all_weekly$TIMESTAMP_START, format = "%V")
-head(all_weekly)
+# load site data ####
+sites = list.files("./", pattern = "_WW_", recursive = TRUE, full.names = TRUE)
+load = function(x){
+  read.csv(sites[x]) %>% 
+    dplyr::select(TIMESTAMP_START, NEE_VUT_REF, NEE_VUT_REF_QC, NEE_VUT_REF_RANDUNC) %>% 
+    filter(NEE_VUT_REF_QC != 0)
+}
+arc = load(1)
+arc$site = "ARM"
+arc$neon = "KONZ"
+ha = load(2)
+ha$site = "Harvard"
+ha$neon = "BART"
+ks = load(3)
+ks$site = "Kennedy"
+ks$neon = "OSBS"
+lww = load(4)
+lww$site = "Washita"
+lww$neon = "KONZ"
+src = load(5)
+src$site = "SRC"
+src$neon = "SRER"
+srg = load(6)
+srg$site = "SRG"
+srg$neon = "SRER"
+srm = load(7)
+srm$site = "SRM"
+srm$neon = "SRER"
+
+all = rbind(arc, ha, ks, lww, src, srg, srm)
+head(all)
+all$TIMESTAMP_START = as.Date(as.character(all$TIMESTAMP_START), format = "%Y%m%d")
+all$week = strftime(all$TIMESTAMP_START, format = "%V")
+head(all)
 
 # calculate GPP for day of the year
-all_agg = aggregate(data = all_weekly, NEE_VUT_REF ~ week + site, FUN = function(x) c(avg = mean(x), upper=mean(x)+sd(x)/sqrt(length(x)),lower=mean(x)-sd(x)/sqrt(length(x))))
+all_agg = aggregate(data = all, NEE_VUT_REF ~ week + site + neon, FUN = function(x) c(avg = mean(x), upper=mean(x)+sd(x)/sqrt(length(x)),lower=mean(x)-sd(x)/sqrt(length(x))))
 all_agg$week = as.numeric(all_agg$week)
+head(all_agg)
 
 # plots
 p1 = ggplot(data = all_agg, aes(x = week, y = NEE_VUT_REF[,1], ymin = NEE_VUT_REF[,3], ymax = NEE_VUT_REF[,2], fill = site)) +
@@ -37,5 +50,7 @@ p1 = ggplot(data = all_agg, aes(x = week, y = NEE_VUT_REF[,1], ymin = NEE_VUT_RE
   xlab("Week in the Year") +
   ylab("Mean NEE using Variable Ustar Threshold (VUT)")
 p4 =
-  p1 + facet_wrap( ~ site, nrow = 1)
+  p1 + facet_wrap( ~ neon, nrow = 1)
 p4
+
+write.csv(all_agg, "weekly_FLUXNET_NEE.csv", row.names = FALSE)
