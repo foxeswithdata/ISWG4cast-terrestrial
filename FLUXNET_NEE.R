7613d132a47999e37fd206be09008e5e75016317
Kathryn Fuller <90946112@westernsydney.edu.au>
2021-04-19 20:42:00 -0500

    Calculated weekly nighttime, daytime and difference in daily and nightly NEE from hourly data

--- FLUXNET_NEE.R
+++ FLUXNET_NEE.R
@@ -4,10 +4,10 @@ library(tidyverse)
 library(scales)
 
 # load site data ####
-sites = list.files("./", pattern = "_WW_", recursive = TRUE, full.names = TRUE)
+sites = list.files("./", pattern = "_HH_", recursive = TRUE, full.names = TRUE)
 load = function(x){
   read.csv(sites[x]) %>% 
-    dplyr::select(TIMESTAMP_START, NEE_VUT_REF, NEE_VUT_REF_QC, NEE_VUT_REF_RANDUNC) %>% 
+    dplyr::select(TIMESTAMP_START, TIMESTAMP_END, NIGHT, NEE_VUT_REF, NEE_VUT_REF_QC, NEE_VUT_REF_RANDUNC) %>% 
     filter(NEE_VUT_REF_QC != 0)
 }
 arc = load(1)
@@ -34,23 +34,71 @@ srm$neon = "SRER"
 
 all = rbind(arc, ha, ks, lww, src, srg, srm)
 head(all)
-all$TIMESTAMP_START = as.Date(as.character(all$TIMESTAMP_START), format = "%Y%m%d")
+all$TIMESTAMP_START = as.POSIXct(as.character(all$TIMESTAMP_START), format = "%Y%m%d%H%M")
+all$TIMESTAMP_END = as.POSIXct(as.character(all$TIMESTAMP_END), format = "%Y%m%d%H%M")
+all$day = as.Date(substr(all$TIMESTAMP_START, 1, 10))
 all$week = strftime(all$TIMESTAMP_START, format = "%V")
+all$NIGHT = as.factor(all$NIGHT)
 head(all)
 
 # calculate GPP for day of the year
-all_agg = aggregate(data = all, NEE_VUT_REF ~ week + site + neon, FUN = function(x) c(avg = mean(x), upper=mean(x)+sd(x)/sqrt(length(x)),lower=mean(x)-sd(x)/sqrt(length(x))))
+all_agg = aggregate(data = all, NEE_VUT_REF ~ NIGHT + day + week + site + neon, FUN = mean)
+                      # function(x) c(avg = mean(x), upper=mean(x)+sd(x)/sqrt(length(x)),lower=mean(x)-sd(x)/sqrt(length(x))))
 all_agg$week = as.numeric(all_agg$week)
 head(all_agg)
 
+# convert NIGHT column and values to different variables
+all_night = all_agg %>% 
+  filter(NIGHT == 1)
+all_night$nee_night = all_night$NEE_VUT_REF
+all_night = all_night %>% 
+  dplyr::select(day,
+                week,
+                site,
+                neon,
+                nee_night)
+all_day = all_agg %>% 
+  filter(NIGHT == 0)
+all_day$nee_day = all_day$NEE_VUT_REF
+all_day = all_day %>% 
+  dplyr::select(day,
+                week,
+                site,
+                neon,
+                nee_day)
+all = full_join(all_night, all_day)
+all$nee_diff = all$nee_night - all$nee_day
+
+# calculate weekly stats, with combined variation over days of the week and years
+night_agg = aggregate(data = all, nee_night ~ week + site + neon, FUN = function(x) c(avg = mean(x), upper=mean(x)+sd(x)/sqrt(length(x)),lower=mean(x)-sd(x)/sqrt(length(x))))
+day_agg = aggregate(data = all, nee_day ~ week + site + neon, FUN = function(x) c(avg = mean(x), upper=mean(x)+sd(x)/sqrt(length(x)),lower=mean(x)-sd(x)/sqrt(length(x))))
+diff_agg = aggregate(data = all, nee_diff ~ week + site + neon, FUN = function(x) c(avg = mean(x), upper=mean(x)+sd(x)/sqrt(length(x)),lower=mean(x)-sd(x)/sqrt(length(x))))
+
 # plots
-p1 = ggplot(data = all_agg, aes(x = week, y = NEE_VUT_REF[,1], ymin = NEE_VUT_REF[,3], ymax = NEE_VUT_REF[,2], fill = site)) +
-  geom_line() +
-  geom_ribbon(alpha = 0.5) +
-  xlab("Week in the Year") +
-  ylab("Mean NEE using Variable Ustar Threshold (VUT)")
+plt = function(df, var, label) {
+  ggplot(data = df, aes(x = week, y = var[,1], ymin = var[,3], ymax = var[,2], fill = site)) +
+    geom_line() +
+    geom_ribbon(alpha = 0.5) +
+    xlab("Week in the Year") +
+    ylab(paste0("Mean ", label, " using Variable Ustar Threshold (VUT)"))
+}
+# nighttime
+p1 = plt(df = night_agg, var = night_agg$nee_night, label = "nighttime NEE")
+p4 =
+  p1 + facet_wrap( ~ neon, nrow = 1)
+p4
+# daytime
+p1 = plt(df = day_agg, var = day_agg$nee_day, label = "daytime NEE")
+p4 =
+  p1 + facet_wrap( ~ neon, nrow = 1)
+p4
+# GPP analogue
+p1 = plt(df = diff_agg, var = diff_agg$nee_diff, label = "difference in NEE")
 p4 =
   p1 + facet_wrap( ~ neon, nrow = 1)
 p4
 
-write.csv(all_agg, "weekly_FLUXNET_NEE.csv", row.names = FALSE)
+# write data to disk
+write.csv(night_agg, "nighttime_weekly_FLUXNET_NEE.csv", row.names = FALSE)
+write.csv(day_agg, "daytime_weekly_FLUXNET_NEE.csv", row.names = FALSE)
+write.csv(diff_agg, "night-day_weekly_FLUXNET_NEE.csv", row.names = FALSE)
